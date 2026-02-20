145438ce591780aa450c9f131d60b9d50874e188
czepepe-dev <czepepe@gmail.com>
2026-02-20 03:11:26 +0100

    Update auth-callback.js

--- netlify/functions/auth-callback.js
+++ netlify/functions/auth-callback.js
@@ -1,21 +1,28 @@
-export default async (req, context) => {
-  const code = new URL(req.url).searchParams.get("code");
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+  const code = event.queryStringParameters.code;
   const clientId = process.env.GITHUB_CLIENT_ID;
   const clientSecret = process.env.GITHUB_CLIENT_SECRET;
 
-  const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: { "Content-Type": "application/json", "Accept": "application/json" },
-    body: JSON.stringify({
-      client_id: clientId,
-      client_secret: clientSecret,
-      code
-    })
-  });
+  const tokenResponse = await fetch(
+    `https://github.com/login/oauth/access_token`,
+    {
+      method: "POST",
+      headers: { Accept: "application/json" },
+      body: new URLSearchParams({
+        client_id: clientId,
+        client_secret: clientSecret,
+        code,
+      }),
+    }
+  );
 
   const tokenData = await tokenResponse.json();
 
-  return new Response(JSON.stringify(tokenData), {
-    headers: { "Content-Type": "application/json" }
-  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify(tokenData),
+    headers: { "Content-Type": "application/json" },
+  };
 };
