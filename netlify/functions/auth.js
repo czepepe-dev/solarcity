3a51d3bba7686dff4df93c7643a30632ef853c7b
czepepe-dev <czepepe@gmail.com>
2026-02-20 03:36:21 +0100

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,12 +1,13 @@
 exports.handler = async (event, context) => {
   const clientId = process.env.GITHUB_CLIENT_ID;
-  const redirectUri = `${event.headers.origin}/api/auth-callback`;
+  const redirectUri = "https://solarcitycuba.netlify.app/.netlify/functions/auth-callback";
 
   return {
     statusCode: 302,
     headers: {
-      Location: `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo,user`,
+      Location: `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+        redirectUri
+      )}&scope=repo,user`,
     },
   };
 };
-
